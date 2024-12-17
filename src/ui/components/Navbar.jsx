88d421a16877e6a154886f328bee3b1e16b661b2
Christian Rafael Guado Geronimo <crguado@gmail.com>
2024-12-16 19:39:43 -0500

    Navigate push / replace - useNavigate

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,6 +1,14 @@
-import { Link, NavLink } from "react-router";
+import { Link, NavLink, useNavigate } from "react-router";
 
 export const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate("/login", {
+      replace: true,
+    });
+  };
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
       <Link className="navbar-brand" to="/">
@@ -32,7 +40,7 @@ export const Navbar = () => {
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
           <span className="nav-item nav-link text-primary">Christian</span>
-          <button className="nav-item nav-link btn" type="">
+          <button className="nav-item nav-link btn" onClick={handleLogout}>
             Logout
           </button>
         </ul>
