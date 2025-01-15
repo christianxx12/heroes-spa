e58c4f74f5cf230e3eba3f65444ff4047a43c9ff
Christian Rafael Guado Geronimo <crguado@gmail.com>
2025-01-14 23:31:17 -0500

    Mostrar mensajes condicionales

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -11,13 +11,16 @@ export const SearchPage = () => {
   const { q = "" } = queryString.parse(location.search);
   const heroes = getHeroesByName(q);
 
+  const showSearch = (q.length === 0)
+  const showError = (q.length > 0) && heroes.length === 0
+
   const { searchText, onInputChange } = useForm({
     searchText: q,
   });
 
   const onSearchSubmit = (e) => {
     e.preventDefault();
-    if (searchText.trim().length <= 1) return;
+    // if (searchText.trim().length <= 1) return;
 
     navigate(`?q=${searchText}`);
   };
@@ -50,9 +53,13 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-primary">Search a hero</div>
+          <div className="alert alert-primary animate__animated animate__fadeIn"
+            style={{ display: showSearch ? '' : 'none' }}>
+            Search a hero
+          </div>
 
-          <div className="alert alert-danger">
+          <div className="alert alert-danger animate__animated animate__fadeIn"
+            style={{ display: showError ? '' : 'none' }}>
             No hero with <b>{q}</b>
           </div>
 
